c1364dae4870ed592be4696963b5fd16c9718e58
Semagsoft <32625597+Semagsoft@users.noreply.github.com>
2021-07-22 23:07:17 -0500

    2021.0

---
+++ Document.Editor/Dialogs/StartupDialog.xaml.vb
@@ -0,0 +1,62 @@
+﻿Imports AdsJumboWinForm
+Public Class StartupDialog
+
+    Private Sub Window_Loaded(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
+        If My.Computer.Info.OSVersion >= "6.0" Then
+            If My.Settings.Options_EnableGlass Then
+                AppHelper.ExtendGlassFrame(Me, New Thickness(-1, -1, -1, -1))
+            End If
+        End If
+        If My.Settings.Options_ShowStartupDialog Then
+            ShowOnStartupCheckBox.IsChecked = True
+        Else
+            ShowOnStartupCheckBox.IsChecked = False
+        End If
+
+        Dim ba As New BannerAds()
+        Dim ia As New InterstitialAd()
+        'ba.ApplicationId = "27cqmrlhom5k"
+        'ba.WidthAd = 90
+        'ba.HeightAd = 90
+        adHost.Child = ba
+        'adHost_Copy.Child = ia
+        ba.ShowAd(468, 60, "27cqmrlhom5k")
+        'ia.ShowInterstitialAd("27cqmrlhom5k")
+    End Sub
+
+    Private Sub OnlineHelpButton_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles OnlineHelpButton.Click
+        Process.Start("http://documenteditor.net/documentation/")
+    End Sub
+
+    Private Sub GetPluginsButton_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles GetPluginsButton.Click
+        Process.Start("http://documenteditor.net/plugins")
+    End Sub
+
+    Private Sub WebsiteButton_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles WebsiteButton.Click
+        Process.Start("http://documenteditor.net")
+    End Sub
+
+    Private Sub CloseButton_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles CloseButton.Click
+        If ShowOnStartupCheckBox.IsChecked Then
+            My.Settings.Options_ShowStartupDialog = True
+        Else
+            My.Settings.Options_ShowStartupDialog = False
+        End If
+        Close()
+    End Sub
+
+    Private Sub ReportBugButton_Click(sender As Object, e As RoutedEventArgs) Handles ReportBugButton.Click
+        Try
+            Process.Start("mailto:semagsoft@gmail.com")
+        Catch ex As Exception
+            Dim m As New MessageBoxDialog(ex.Message, ex.ToString, Nothing, Nothing)
+            m.MessageImage.Source = New BitmapImage(New Uri("pack://application:,,,/Images/Common/error32.png"))
+            m.Owner = Me
+            m.ShowDialog()
+        End Try
+    End Sub
+
+    Private Sub DonateButton_Click(sender As Object, e As RoutedEventArgs) Handles DonateButton.Click
+        Process.Start("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=K4QJBR4UJ3W5E&lc=US&item_name=Semagsoft&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted")
+    End Sub
+End Class
---
+++ Document.Editor/bin/Debug/Plugins/HelloWorld.vb
@@ -0,0 +1,18 @@
+﻿Public Class HelloWorld
+
+    Public Function IsStartupPlugin() As Boolean
+        Return False
+    End Function
+
+    Public Function IsEventPlugin() As Boolean
+        Return True
+    End Function
+
+    Public Function StartPlugin() As Object
+        Dim i As String = "hello world!", win As New Form
+        win.Text = i
+        win.ShowDialog()
+        Return i
+    End Function
+
+End Class
